f4ee0e1f64df11697cbe2f0a29017f2fc5cae469
Moisés <62753698+moisecas@users.noreply.github.com>
2022-02-05 14:05:02 -0500

    Add files via upload

---
+++ Cajero automatico/Cajero.js
@@ -0,0 +1,66 @@
+class Billete {
+    constructor(v,c)
+    {
+        this.valor=v;
+        this.cantidad=c;
+    }
+
+}
+
+function entregarDinero()
+{
+    var t = document.getElementById("dinero"); 
+    dinero = parseInt(t.value);
+    for(var bi of caja)
+    {
+ 
+        if(dinero > 0) 
+        {
+            div= Math.floor(dinero/bi.valor); 
+            if(div > bi.cantidad) 
+            {
+                papeles = bi.cantidad; 
+            }
+            else {
+                papeles= div; 
+            }
+
+            entregado.push(new Billete(bi.valor, papeles)); 
+            dinero = dinero - (bi.valor*papeles); 
+        }
+    } 
+
+    if(dinero > 0)
+    {
+        resultado.innerHTML= "LE TOCO IR A OTRO CAJERO :V";
+         
+    }
+    else 
+    {
+        for(var e of entregado) 
+        {
+
+
+            resultado.innerHTML= resultado.innerHTML + e.cantidad + " billetes de $" + e.valor + "<br />"; 
+
+        } 
+    }
+
+    console.log(entregado); 
+}
+
+var caja =[];
+var entregado =[];
+caja.push( new Billete(50, 10) );
+caja.push( new Billete(20, 2) );
+caja.push( new Billete(10, 2) );
+caja.push( new Billete(5, 2) ); 
+
+var dinero = 0; 
+var div= 0;
+var papeles= 0;  
+
+var b = document.getElementById("extraer");
+document.addEventListener("click", entregarDinero); 
+
+var resultado = document.getElementById("resultado"); 
